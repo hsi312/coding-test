07576d545a9c145c67fd46268f4e838c7d105d25
lignas <alwaysone312@gmail.com>
2025-07-05 18:52:41 +0900

    주석추가

---
+++ src/main/java/com/example/demo/practice/Main.java
@@ -0,0 +1,14 @@
+package com.example.demo.practice;
+
+import java.util.*;
+
+public class Main {
+
+    public void solution(String str) {
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+    }
+}
