0d6eabd88e0b3f30d699c9b9c30bb01000dc51c8
lignas <alwaysone312@gmail.com>
2025-07-07 21:42:09 +0900

    String 7~11

--- src/main/java/com/example/demo/practice/Main.java
+++ src/main/java/com/example/demo/practice/Main.java
@@ -4,11 +4,29 @@ import java.util.*;
 
 public class Main {
 
-    public void solution(String str) {
+    public void solution(String s) {
+        s = s + " ";
+        char[] c = s.toCharArray();
+        StringBuilder builder = new StringBuilder();
+        int cnt = 1;
+        for (int i = 0; i < c.length - 1; i++) {
+            if (c[i] == c[i + 1]) {
+                cnt++;
+            } else {
+                builder.append(c[i]);
+                if (cnt > 1) {
+                    builder.append(cnt);
+                }
+                cnt = 1;
+            }
+        }
+        System.out.println(builder);
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        T.solution(s);
     }
 }
