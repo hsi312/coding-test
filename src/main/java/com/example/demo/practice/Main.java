57b1d72e84d678c5420e209b4a37e9e325f01cb2
lignas <alwaysone312@gmail.com>
2025-07-09 21:44:02 +0900

    String 12, Array 1~4

--- src/main/java/com/example/demo/practice/Main.java
+++ src/main/java/com/example/demo/practice/Main.java
@@ -4,29 +4,28 @@ import java.util.*;
 
 public class Main {
 
-    public void solution(String s) {
-        s = s + " ";
-        char[] c = s.toCharArray();
-        StringBuilder builder = new StringBuilder();
-        int cnt = 1;
-        for (int i = 0; i < c.length - 1; i++) {
-            if (c[i] == c[i + 1]) {
-                cnt++;
-            } else {
-                builder.append(c[i]);
-                if (cnt > 1) {
-                    builder.append(cnt);
-                }
-                cnt = 1;
-            }
+    public ArrayList<Integer> solution(int n) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int a = 1;
+        int b = 1;
+        answer.add(a);
+        answer.add(b);
+        for (int i = 2; i < n; i++) {
+            int sum = a + b;
+            answer.add(sum);
+            a = b;
+            b = sum;
         }
-        System.out.println(builder);
+
+        return answer;
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        String s = sc.next();
-        T.solution(s);
+        int n = sc.nextInt();
+        for (int x : T.solution(n)) {
+            System.out.print(x + " ");
+        }
     }
 }
