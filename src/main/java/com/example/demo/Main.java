8972dabec1589a6fcade245d4542521aa1735cf8
crinity-hsi <hsi@crinity.com>
2025-07-29 12:53:15 +0900

    stack 3. 카카오 인형뽑기

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -4,28 +4,43 @@ import java.util.*;
 
 public class Main {
 
-    public String solution(String str) {
-        String answer = "";
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) != ')') {
-                stack.add(str.charAt(i));
-            } else {
-                while (stack.pop() != '(');
+    public int solution(int n, int[][] board, int m, int[] moves) {
+        Stack<Integer> stack = new Stack<>();
+        int cnt = 0;
+        for (int col : moves) {
+            for (int row = 1; row < n; row++) {
+                if (board[row][col] != 0) {
+                    if (!stack.isEmpty() && stack.peek() == board[row][col]) {
+                        stack.pop();
+                        cnt += 2;
+                        board[row][col] = 0;
+                        break;
+                    }
+                    stack.push(board[row][col]);
+                    board[row][col] = 0;
+                    break;
+                }
             }
         }
 
-        for (char c : stack) {
-            answer += c;
-        }
-
-        return answer;
+        return cnt;
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        System.out.println(T.solution(str));
+        int n = sc.nextInt() + 1;
+        int[][] board = new int[n][n];
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+        int m = sc.nextInt();
+        int[] moves = new int[m];
+        for (int i = 0; i < m; i++) {
+            moves[i] = sc.nextInt();
+        }
+        System.out.println(T.solution(n, board, m, moves));
     }
 }
