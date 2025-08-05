0091e1c6be6b507319ad2dc6bb5267cee01d08db
lignas <alwaysone312@gmail.com>
2025-08-05 21:58:29 +0900

    stack queue 5. 쇠막대기 stack 6. 공주 구하기 queue 7. 교육과정 설계 queue 8. 응급실 queue

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -2,30 +2,52 @@ package com.example.demo;
 
 import java.util.*;
 
+class Person {
+    int id;
+    int priority;
+    Person (int id, int priority) {
+        this.id = id;
+        this.priority = priority;
+    }
+}
+
 public class Main {
 
-    public int solution(String str) {
-        Deque<Integer> stack = new ArrayDeque<>();
-        for (char c : str.toCharArray()) {
-            if (Character.isDigit(c)) {
-                stack.push(Character.getNumericValue(c));
-            } else {
-                int num2 = stack.pop();
-                int num1 = stack.pop();
-                if (c == '+') stack.push(num1 + num2);
-                else if (c == '-') stack.push(num1 - num2);
-                else if (c == '*') stack.push(num1 * num2);
-                else if (c == '/') stack.push(num1 / num2);
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        Queue<Person> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            q.offer(new Person(i, arr[i]));
+        }
+        while (!q.isEmpty()) {
+            Person tmp = q.poll();
+            for (Person p : q) {
+                if (p.priority > tmp.priority) {
+                    q.offer(tmp);
+                    tmp = null;
+                    break;
+                }
+            }
+            if (tmp != null) {
+                answer++;
+                if (tmp.id == m) {
+                    return answer;
+                }
             }
         }
 
-        return stack.pop();
+        return answer;
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        System.out.println(T.solution(str));
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(T.solution(n, m, arr));
     }
 }
