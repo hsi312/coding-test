5b9f2e20250b82fcbf8aae3348dfb01acc2d1366
crinity-hsi <hsi@crinity.com>
2025-08-05 12:51:15 +0900

    stack(Deque) 4. 후위식 연산

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -4,43 +4,28 @@ import java.util.*;
 
 public class Main {
 
-    public int solution(int n, int[][] board, int m, int[] moves) {
-        Stack<Integer> stack = new Stack<>();
-        int cnt = 0;
-        for (int col : moves) {
-            for (int row = 1; row < n; row++) {
-                if (board[row][col] != 0) {
-                    if (!stack.isEmpty() && stack.peek() == board[row][col]) {
-                        stack.pop();
-                        cnt += 2;
-                        board[row][col] = 0;
-                        break;
-                    }
-                    stack.push(board[row][col]);
-                    board[row][col] = 0;
-                    break;
-                }
+    public int solution(String str) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c)) {
+                stack.push(Character.getNumericValue(c));
+            } else {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                if (c == '+') stack.push(num1 + num2);
+                else if (c == '-') stack.push(num1 - num2);
+                else if (c == '*') stack.push(num1 * num2);
+                else if (c == '/') stack.push(num1 / num2);
             }
         }
 
-        return cnt;
+        return stack.pop();
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt() + 1;
-        int[][] board = new int[n][n];
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < n; j++) {
-                board[i][j] = sc.nextInt();
-            }
-        }
-        int m = sc.nextInt();
-        int[] moves = new int[m];
-        for (int i = 0; i < m; i++) {
-            moves[i] = sc.nextInt();
-        }
-        System.out.println(T.solution(n, board, m, moves));
+        String str = sc.next();
+        System.out.println(T.solution(str));
     }
 }
