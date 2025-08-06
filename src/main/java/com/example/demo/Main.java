5cd0cd087924f742f8a94adca961165809a25e7a
lignas <alwaysone312@gmail.com>
2025-08-06 22:02:06 +0900

    sort 3. 삽입 정렬 4. Least Recently Used(캐시 히트, 미스)

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -4,29 +4,41 @@ import java.util.*;
 
 public class Main {
 
-    public int[] solution(int n, int[] arr) {
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
+    public int[] solution(int size, int n, int[] arr) {
+        int[] cache = new int[size];
+        for (int x : arr) {
+            int pos = -1;
+            for (int i = 0; i < size; i++) {
+                if (x == cache[i]) {
+                    pos = i;
                 }
             }
+            if (pos == -1) {
+                for (int i = size - 1; i >= 1; i--) {
+                    cache[i] = cache[i - 1];
+                }
+            } else {
+                for (int  i = pos; i >= 1; i--) {
+                    cache[i] = cache[i - 1];
+                }
+            }
+            cache[0] = x;
         }
 
-        return arr;
+        return cache;
+
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
         int n = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        for (int i : T.solution(n, arr)) {
+        for (int i : T.solution(s, n, arr)) {
             System.out.print(i + " ");
         }
     }
