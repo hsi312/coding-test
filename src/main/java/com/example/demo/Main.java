a6a15338c1ff257ab22317decf52341cf41adfe5
lignas <alwaysone312@gmail.com>
2025-08-07 21:31:53 +0900

    sort 7. 좌표 정렬(Comparable<Point>) 8. 이분검색(lt, rt, mid) 9. 뮤지비디오(결정 알고리즘)

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -4,13 +4,31 @@ import java.util.*;
 
 public class Main {
 
-    public ArrayList<Integer> solution(int n, int[] arr) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        int[] clone = arr.clone();
-        Arrays.sort(clone);
-        for (int i = 0; i < n; i++) {
-            if (arr[i] != clone[i]) {
-                answer.add(i + 1);
+    public int count(int[] arr, int capacity) {
+        int cnt = 1;
+        int sum = 0;
+        for (int i : arr) {
+            if (sum + i > capacity) {
+                cnt++;
+                sum = i;
+            } else {
+                sum += i;
+            }
+        }
+        return cnt;
+    }
+
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) <= m) {
+                answer = mid;
+                rt = mid - 1;
+            } else {
+                lt = mid + 1;
             }
         }
 
@@ -21,12 +39,11 @@ public class Main {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int m = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        for (int i : T.solution(n, arr)) {
-            System.out.print(i + " ");
-        }
+        System.out.println(T.solution(n, m, arr));
     }
 }
