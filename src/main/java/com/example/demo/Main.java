c11b972af2ff2eb9e8ec8c2ee333b4b3ca78b4e0
crinity-hsi <hsi@crinity.com>
2025-08-11 12:54:39 +0900

    DFS 재귀함수 1. 재귀함수 2. 이진수 출력 3. 팩토리얼

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -3,47 +3,17 @@ package com.example.demo;
 import java.util.*;
 
 public class Main {
+    public static int[] fibo;
+    public int DFS(int n) {
 
-    public int count(int[] arr, int mid) {
-        int cnt = 1;
-        int ep = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] - ep >= mid) {
-                cnt++;
-                ep = arr[i];
-            }
-        }
-
-        return cnt;
-    }
-
-    public int solution(int n, int c, int[] arr) {
-        int answer = 0;
-        Arrays.sort(arr);
-        int lt = 1;
-        int rt = arr[n-1] - 1;
-        while (lt <= rt) {
-            int mid = (lt + rt) / 2;
-            if (count(arr, mid) >= c) {
-                answer = mid;
-                lt = mid + 1;
-            } else {
-                rt = mid - 1;
-            }
-        }
-
-        return answer;
+        return 0;
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int c = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        System.out.println(T.solution(n, c, arr));
+        fibo = new int[n];
+        System.out.println(T.DFS(n));
     }
 }
