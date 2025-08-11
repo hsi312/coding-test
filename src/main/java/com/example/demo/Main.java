7409aecfc3f14407b2434319277b4303d785607d
lignas <alwaysone312@gmail.com>
2025-08-11 21:40:35 +0900

    DFS (깊이우선탐색) 피보나치수열 이진트리순회(전위순회, 중위순회, 후위순회)

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -2,18 +2,40 @@ package com.example.demo;
 
 import java.util.*;
 
-public class Main {
-    public static int[] fibo;
-    public int DFS(int n) {
+class Node {
+    int data;
+    Node lt;
+    Node rt;
+    public Node(int data) {
+        this.data = data;
+        lt = null;
+        rt = null;
+    }
+}
 
-        return 0;
+public class Main {
+    Node root;
+
+    public void DFS(Node root) {
+        if (root == null) {
+            return;
+        } else {
+            DFS(root.lt);
+            DFS(root.rt);
+            System.out.print(root.data + " ");
+        }
     }
 
     public static void main(String[] args) {
-        Main T = new Main();
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        fibo = new int[n];
-        System.out.println(T.DFS(n));
+        Main tree = new Main();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        tree.root.rt.lt = new Node(6);
+        tree.root.rt.rt = new Node(7);
+
+        tree.DFS(tree.root);
     }
 }
