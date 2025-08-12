177319ae3e4b3ba4778ec98afe9ac6dfc15008aa
lignas <alwaysone312@gmail.com>
2025-08-12 21:24:41 +0900

    BFS (넓이우선탐색 레벨탐색) Queue 이진트리 순회 송아지 찾기

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -3,25 +3,38 @@ package com.example.demo;
 import java.util.*;
 
 public class Main {
-    public static int[] fibo;
-    public static int n;
 
-    public int DFS(int n) {
-        if (n == 1) return fibo[n] = 1;
-        else if (n == 2) return fibo[n] = 1;
-        else {
-            return fibo[n] = DFS(n - 2) + DFS(n - 1);
+    public int BFS(int s, int e) {
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(s);
+        int[] ch = new int[10001];
+        int[] dis = {1, -1, 5};
+        int L = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                int cur = q.poll();
+                for (int j : dis) {
+                    int tmp = cur + j;
+                    if (tmp == e) {
+                        return L + 1;
+                    }
+                    if (tmp >= 1 && tmp <= 10000 && ch[tmp] == 0) {
+                        ch[tmp] = 1;
+                        q.offer(tmp);
+                    }
+                }
+            }
+            L++;
         }
+        return L;
     }
 
     public static void main(String[] args) {
         Main m = new Main();
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        fibo = new int[n + 1];
-        m.DFS(n);
-        for (int i = 1; i <= n; i++) {
-            System.out.print(fibo[i] + " ");
-        }
+        int s = sc.nextInt();
+        int e = sc.nextInt();
+        System.out.println(m.BFS(s, e));
     }
 }
