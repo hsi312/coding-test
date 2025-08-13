56d68210d47e6e4df1e489ebafb360285e45518a
crinity-hsi <hsi@crinity.com>
2025-08-13 12:47:40 +0900

    Tree 말단 노드까지 가장 짧은 경로(DFS) Tree 말단 노드까지 가장 짧은 경로(BFS)
    그래프와 인접행렬
    무방향 그래프
    방향 그래프
    가중치 방향 그래프

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -2,27 +2,33 @@ package com.example.demo;
 
 import java.util.*;
 
+class Node {
+    int data;
+    Node lt;
+    Node rt;
+    public Node(int data) {
+        this.data = data;
+        lt = null;
+        rt = null;
+    }
+}
 public class Main {
-
-    public int BFS(int s, int e) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(s);
-        int[] ch = new int[10001];
-        int[] dis = {1, -1, 5};
-        int L = 0;
-        while (!q.isEmpty()) {
+    Node root;
+    public int BFS(int L, Node root) {
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()) {
             int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int cur = q.poll();
-                for (int j : dis) {
-                    int tmp = cur + j;
-                    if (tmp == e) {
-                        return L + 1;
-                    }
-                    if (tmp >= 1 && tmp <= 10000 && ch[tmp] == 0) {
-                        ch[tmp] = 1;
-                        q.offer(tmp);
-                    }
+            for(int i = 0; i < size; i++) {
+                Node cur = q.poll();
+                if (cur.lt == null && cur.rt == null) {
+                    return L;
+                }
+                if(cur.lt != null) {
+                    q.offer(cur.lt);
+                }
+                if(cur.rt != null) {
+                    q.offer(cur.rt);
                 }
             }
             L++;
@@ -32,9 +38,14 @@ public class Main {
 
     public static void main(String[] args) {
         Main m = new Main();
-        Scanner sc = new Scanner(System.in);
-        int s = sc.nextInt();
-        int e = sc.nextInt();
-        System.out.println(m.BFS(s, e));
+        m.root = new Node(1);
+        m.root.lt = new Node(2);
+        m.root.rt = new Node(3);
+        m.root.lt.lt = new Node(4);
+        m.root.lt.rt = new Node(5);
+        m.root.rt.lt = new Node(6);
+        m.root.rt.rt = new Node(7);
+        m.root.rt.lt.lt = new Node(8);
+        System.out.println(m.BFS(0, m.root));
     }
 }
