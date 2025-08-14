3b70c64d0d3f1ff835f6b123021daf8dffda0565
crinity-hsi <hsi@crinity.com>
2025-08-14 12:48:57 +0900

    DFS 경로탐색(인접리스트, ArrayList)

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -2,50 +2,45 @@ package com.example.demo;
 
 import java.util.*;
 
-class Node {
-    int data;
-    Node lt;
-    Node rt;
-    public Node(int data) {
-        this.data = data;
-        lt = null;
-        rt = null;
-    }
-}
 public class Main {
-    Node root;
-    public int BFS(int L, Node root) {
-        Queue<Node> q = new LinkedList<>();
-        q.offer(root);
-        while(!q.isEmpty()) {
-            int size = q.size();
-            for(int i = 0; i < size; i++) {
-                Node cur = q.poll();
-                if (cur.lt == null && cur.rt == null) {
-                    return L;
-                }
-                if(cur.lt != null) {
-                    q.offer(cur.lt);
-                }
-                if(cur.rt != null) {
-                    q.offer(cur.rt);
+    static int n;
+    static int m;
+    static int answer = 0;
+    static int[] ch;
+    static ArrayList<ArrayList<Integer>> graph;
+
+    public void DFS(int v) {
+        if (n == v) {
+            answer++;
+        } else {
+            for (int nv : graph.get(v)) {
+                if (ch[nv] == 0) {
+                    ch[nv] = 1;
+                    DFS(nv);
+                    ch[nv] = 0;
                 }
             }
-            L++;
         }
-        return L;
     }
 
     public static void main(String[] args) {
-        Main m = new Main();
-        m.root = new Node(1);
-        m.root.lt = new Node(2);
-        m.root.rt = new Node(3);
-        m.root.lt.lt = new Node(4);
-        m.root.lt.rt = new Node(5);
-        m.root.rt.lt = new Node(6);
-        m.root.rt.rt = new Node(7);
-        m.root.rt.lt.lt = new Node(8);
-        System.out.println(m.BFS(0, m.root));
+        Main T = new Main();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        ch = new int[n + 1];
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            int row = sc.nextInt();
+            int col = sc.nextInt();
+            graph.get(row).add(col);
+        }
+        ch[1] = 1;
+        T.DFS(1);
+        System.out.println(answer);
     }
 }
