96f5437e71f156a164173239019e9741082714f4
lignas <alwaysone312@gmail.com>
2025-08-18 22:20:45 +0900

    1. 그래프 최단거리(BFS, 레벨탐색, 인접 리스트) 2. 바둑이 승차(DFS) 3. 최대점수 구하기(DFS)

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -5,23 +5,19 @@ import java.util.*;
 public class Main {
     static int n;
     static int m;
-    static int[] dis;
-    static int[] ch;
-    static ArrayList<ArrayList<Integer>> graph;
+    static int answer = Integer.MIN_VALUE;
+    static int[] time;
+    static int[] score;
 
-    public void BFS(int v) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(v);
-
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            for (int nv : graph.get(cur)) {
-                if (ch[nv] == 0) {
-                    ch[nv] = 1;
-                    dis[nv] = dis[cur] + 1;
-                    q.add(nv);
-                }
-            }
+    public void DFS(int L, int sum, int t) {
+        if (t > m) {
+            return;
+        }
+        if (L == n) {
+            answer = Math.max(sum, answer);
+        } else {
+            DFS(L + 1, sum + score[L], time[L] + t);
+            DFS(L + 1, sum, t);
         }
     }
 
@@ -30,21 +26,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
-        dis = new int[n + 1];
-        ch = new int[n + 1];
-        graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
+        time = new int[n];
+        score = new int[n];
+        for (int i = 0; i < n; i++) {
+            score[i] = sc.nextInt();
+            time[i] = sc.nextInt();
         }
-        for (int i = 0; i < m; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            graph.get(a).add(b);
-        }
-        ch[1] = 1;
-        T.BFS(1);
-        for (int i = 2; i <= n; i++) {
-            System.out.println(i +  " : " + dis[i]);
-        }
+        T.DFS(0, 0, 0);
+        System.out.println(answer);
     }
 }
