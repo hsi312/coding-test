9cdb3bc2f006dcb54644703739b0c50bb11a9bb0
crinity-hsi <hsi@crinity.com>
2025-08-18 13:21:45 +0900

    BFS 그래프 최단거리(인접리스트, ArrayList)

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -5,19 +5,21 @@ import java.util.*;
 public class Main {
     static int n;
     static int m;
-    static int answer = 0;
+    static int[] dis;
     static int[] ch;
     static ArrayList<ArrayList<Integer>> graph;
 
-    public void DFS(int v) {
-        if (n == v) {
-            answer++;
-        } else {
-            for (int nv : graph.get(v)) {
+    public void BFS(int v) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(v);
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            for (int nv : graph.get(cur)) {
                 if (ch[nv] == 0) {
                     ch[nv] = 1;
-                    DFS(nv);
-                    ch[nv] = 0;
+                    dis[nv] = dis[cur] + 1;
+                    q.add(nv);
                 }
             }
         }
@@ -28,19 +30,21 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
         m = sc.nextInt();
+        dis = new int[n + 1];
         ch = new int[n + 1];
         graph = new ArrayList<>();
         for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
-
         for (int i = 0; i < m; i++) {
-            int row = sc.nextInt();
-            int col = sc.nextInt();
-            graph.get(row).add(col);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph.get(a).add(b);
         }
         ch[1] = 1;
-        T.DFS(1);
-        System.out.println(answer);
+        T.BFS(1);
+        for (int i = 2; i <= n; i++) {
+            System.out.println(i +  " : " + dis[i]);
+        }
     }
 }
