8da1e36f844ae656f782b53c74fd55743d78cdb3
lignas <alwaysone312@gmail.com>
2025-08-25 21:55:27 +0900

    DFS 수열 추측하기

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -3,17 +3,37 @@ package com.example.demo;
 import java.util.*;
 
 public class Main {
-    static int n, r;
-    int[][] answer = new int[34][34];
+    static int n, f;
+    static int[] p, ch, b;
+    int[][] dy = new int[17][17];
+    boolean flag = false;
 
-    public int DFS(int n, int r) {
-        if (answer[n][r] > 0) {
-            return answer[n][r];
-        }
+    public int combi(int n, int r) {
         if (n == r || r == 0) {
             return 1;
         } else {
-            return answer[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r);
+            return dy[n][r] = combi(n - 1, r - 1) + combi(n - 1, r);
+        }
+    }
+
+    public void DFS(int L, int sum) {
+        if (flag) return;
+        if (L == n) {
+            if (sum == f) {
+                for (int i : p) {
+                    System.out.print(i + " ");
+                }
+                flag = true;
+            }
+        } else {
+            for (int i  = 1; i <= n; i++) {
+                if (ch[i] == 0) {
+                    ch[i] = 1;
+                    p[L] = i;
+                    DFS(L + 1, sum + (p[L] * b[L]));
+                    ch[i] = 0;
+                }
+            }
         }
     }
 
@@ -21,7 +41,13 @@ public class Main {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
-        r = sc.nextInt();
-        System.out.println(T.DFS(n, r));
+        f = sc.nextInt();
+        ch = new int[n + 1];
+        p = new int[n];
+        b = new int[n];
+        for (int i = 0; i < n; i++) {
+            b[i] = T.combi(n - 1, i);
+        }
+        T.DFS(0, 0);
     }
 }
