5087a91ab62946fa1a3c3584f1b2db30a8450452
crinity-hsi <hsi@crinity.com>
2025-08-29 12:57:49 +0900

    DFS 섬나라 아일랜드

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -2,31 +2,20 @@ package com.example.demo;
 
 import java.util.*;
 
-class Pointer {
-    public int x;
-    public int y;
-    Pointer(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
 public class Main {
-    static int m, n, day = Integer.MIN_VALUE;
-    static int[][] graph, dist;
-    int[] dx = { -1, 0, 1, 0 };
-    int[] dy = { 0, 1, 0, -1 };
-    static Queue<Pointer> q = new LinkedList<>();
+    static int n, answer;
+    static int[][] board;
+    int[] dx = { -1, -1, 0, 1, 1, 1, 0, -1 };
+    int[] dy = { 0, 1, 1, 1, 0, -1, -1, -1 };
 
-    public void BFS() {
-        while (!q.isEmpty()) {
-            Pointer cur = q.poll();
-            for (int i = 0; i < 4; i++) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && graph[nx][ny] == 0) {
-                    graph[nx][ny] = 1;
-                    q.offer(new Pointer(nx, ny));
-                    dist[nx][ny] = dist[cur.x][cur.y] + 1;
+    public void DFS(int x, int y) {
+        if (board[x][y] == 1) {
+            board[x][y] = 0;
+            for (int k = 0; k < 8; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1) {
+                    DFS(nx, ny);
                 }
             }
         }
@@ -35,33 +24,21 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        m = sc.nextInt();
         n = sc.nextInt();
-        graph = new int[n][m];
-        dist = new int[n][m];
+        board = new int[n][n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int num = sc.nextInt();
-                graph[i][j] = num;
-                if (num == 1) {
-                    q.offer(new Pointer(i, j));
-                }
+            for (int j = 0; j < n; j++) {
+                board[i][j] = sc.nextInt();
             }
         }
-        T.BFS();
-        boolean flag = false;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (graph[i][j] == 0) {
-                    flag = true;
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1) {
+                    answer++;
                 }
-                day = Math.max(day, dist[i][j]);
+                T.DFS(i, j);
             }
         }
-        if (flag) {
-            System.out.println(-1);
-        } else {
-            System.out.println(day);
-        }
+        System.out.println(answer);
     }
 }
