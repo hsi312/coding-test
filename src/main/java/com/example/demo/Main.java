de9166c81bf962854a9aba9a4dec53d4e2979967
lignas <alwaysone312@gmail.com>
2025-09-01 21:40:31 +0900

    greedy 씨름 선수 선출(키, 몸무게) 회의실 배정

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -2,59 +2,46 @@ package com.example.demo;
 
 import java.util.*;
 
-class Point {
-    int x;
-    int y;
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+class Time implements Comparable<Time>{
+    int start;
+    int end;
+    Time(int h, int w) {
+        this.start = h;
+        this.end = w;
+    }
+    @Override
+    public int compareTo(Time o) {
+        if (end == o.end) {
+            return this.start - o.start;
+        } else {
+            return this.end - o.end;
+        }
     }
 }
 
 public class Main {
-    static int n, m, len, answer = Integer.MAX_VALUE;
-    static ArrayList<Point> home, pizza;
-    static int[] combination;
 
-    public void DFS(int L, int s) {
-        if (L == m) {
-            int sum = 0;
-            for (Point h : home) {
-                int dis = Integer.MAX_VALUE;
-                for (int c : combination) {
-                    dis = Math.min(dis, Math.abs(h.x - pizza.get(c).x) + Math.abs(h.y - pizza.get(c).y));
-                }
-                sum += dis;
-            }
-            answer = Math.min(answer, sum);
-        } else {
-            for (int i = s; i < len; i++) {
-                combination[L] = i;
-                DFS(L + 1, i + 1);
+    public int solution(ArrayList<Time> arr) {
+        int cnt = 0;
+        int endTime = 0;
+        Collections.sort(arr);
+        for (Time t : arr) {
+            if (t.start >= endTime) {
+                endTime = t.end;
+                cnt++;
             }
         }
+        return cnt;
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        home = new ArrayList<>();
-        pizza = new ArrayList<>();
+        int n = sc.nextInt();
+        ArrayList<Time> arr = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int tmp = sc.nextInt();
-                if (tmp == 1) {
-                    home.add(new Point(i, j));
-                } else if (tmp == 2) {
-                    pizza.add(new Point(i, j));
-                }
-            }
+            arr.add(new Time(sc.nextInt(), sc.nextInt()));
         }
-        len = pizza.size();
-        combination = new int[m];
-        T.DFS(0, 0);
-        System.out.println(answer);
+        System.out.println(T.solution(arr));
     }
 }
